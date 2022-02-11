df565ab223cf243928909affbc73de4e71d4f7a3
matheus.guerra <matheusguerra@outlook.com>
2022-02-10 23:22:19 -0300

    Realiza incrição

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,8 +1,30 @@
+from django.contrib import messages
+from django.core import mail
+from django.http import HttpResponseRedirect
+from django.template.loader import render_to_string
 from django.shortcuts import render
 
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
-    context = {'form': SubscriptionForm()}
-    return render(request, 'subscriptions/subscription_form.html', context)
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+
+        if form.is_valid():
+            bady = render_to_string('subscriptions/subscription_email.txt',
+                                    form.cleaned_data)
+            mail.send_mail('Confirmação de inscrição',
+                           bady,
+                           'contato@eventex.com.br',
+                           ['contato@eventex.com.br', form.cleaned_data['email']])
+
+            messages.success(request, 'Incrição realizada com sucesso!')
+
+            return(HttpResponseRedirect('/inscricao/'))
+        else:
+            return render(request, 'subscriptions/subscription_form.html',
+                          {'form': form})
+    else:
+        context = {'form': SubscriptionForm()}
+        return render(request, 'subscriptions/subscription_form.html', context)
